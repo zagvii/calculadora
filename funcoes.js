3c0f0311a60ae7c0286cb4d7f3d831d9e12bc336
= <=>
2023-03-28 21:53:18 -0300

    undefined

--- funcoes.js
+++ funcoes.js
@@ -1,29 +1,27 @@
-document.getElementById("input").setAttribute('size', '20');
-
 function apagar(){
 
     if (document.getElementById("historico").style.visibility == "hidden" && document.form.input.value != " "){
         document.form.input.value = " ";
-
     } else if (document.getElementById("historico").style.visibility == "visible" && document.form.input.value != " "){
         document.form.input.value = " ";
     } else if (document.getElementById("historico").style.visibility == "visible" && document.form.input.value == " "){
         document.getElementById("historico").style.visibility = "hidden";
-        }
     }
+}
 
-    function equal(){
+function equal(){
         
-        var calculo = document.form.input.value;
+    var calculo = document.form.input.value;
 
+    if(calculo){
         try {
             expressao = eval(calculo);
             historico = calculo;
             visibilidade = "visible";
             cor = "inherit";
-
+    
             eval(calculo); 
-
+    
         } catch (e) {
             if (e instanceof SyntaxError) {
                 expressao = "ERRO";
@@ -31,16 +29,17 @@ function apagar(){
                 visibilidade = "hidden";
                 cor = "red";
             }
-
+    
         } finally {
             document.getElementById("historico").style.visibility = visibilidade;
             document.getElementById("historico").textContent = historico;
             document.getElementById('input').style.color = cor;
             document.form.input.value = expressao;
-
+    
             setTimeout(myTimer, 4000);
             function myTimer() {
                 document.getElementById('input').style.color = 'black';
             }
         }
     }
+}
